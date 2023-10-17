424eb7eb0c7b8b77d7c25a5c910668b24fed6b84
Taehyun Sung <alexseong1004@gmail.com>
2023-10-18 01:06:53 +0800

    feat: working on API still,

---
+++ SimpleWeather/Models/ViewModels/MainPageView.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SimpleWeather.Models.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWeather.Models.ViewModels
+{
+    internal partial class MainPageView : ObservableObject
+    {
+        private readonly WeatherAPIService _weatherApiService;
+
+        public MainPageView()
+        {
+            _weatherApiService = new WeatherAPIService();
+        }
+
+        [ObservableProperty]
+        private string city;
+
+        [ObservableProperty]
+        private float temperature;
+
+        [RelayCommand]
+        private async Task FetchWeatherInformation()
+        {
+            var weatherApiResponse = await _weatherApiService.GetWeatherInformation();
+            if (weatherApiResponse != null)
+            {
+                Temperature = weatherApiResponse.list[0].main.temp;
+            }
+        }
+    }
+}
--- SimpleWeather/WeatherAPIService.cs
+++ SimpleWeather/WeatherAPIService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleWeather.Models.ApiModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
@@ -7,28 +8,46 @@ using System.Threading.Tasks;
 
 namespace SimpleWeather
 {
-    static class WeatherAPIService
+    internal class WeatherAPIService
     {
-        static string baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=";
+        private readonly HttpClient _httpClient;
 
-        public static async Task<List<string>> GetTemp ()
+        public WeatherAPIService()
         {
-            List<string> temps = new List<string>();
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https0://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=");
 
-            string endpoint = baseURL + "temp";
-
-            HttpClient client = new HttpClient();
-
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endpoint);
-            request.Headers.Add("apikey", "557e4ffb0a6ad8eb685bb13051d4a230");
-            HttpResponseMessage response = await client.SendAsync(request);
-
-            if(response.IsSuccessStatusCode)
-            {
-                WeatherResponseModel model = (WeatherResponseModel)await response.Content.ReadFromJsonAsync(typeof(WeatherResponseModel));
-            }
-
-            return temps;
         }
+
+        public async Task<WeatherApiResponse> GetWeatherInformation()
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return null;
+
+            return await _httpClient.GetFromJsonAsync<WeatherApiResponse>("557e4ffb0a6ad8eb685bb13051d4a230");
+        }
+        
+
+        //static string baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=";
+
+        //public static async Task<List<string>> GetTemp ()
+        //{
+        //    List<string> temps = new List<string>();
+
+        //    string endpoint = baseURL + "temp";
+
+        //    HttpClient client = new HttpClient();
+
+        //    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+        //    request.Headers.Add("apikey", "557e4ffb0a6ad8eb685bb13051d4a230");
+        //    HttpResponseMessage response = await client.SendAsync(request);
+
+        //    if(response.IsSuccessStatusCode)
+        //    {
+        //        WeatherResponseModel model = (WeatherResponseModel)await response.Content.ReadFromJsonAsync(typeof(WeatherResponseModel));
+        //    }
+
+        //    return temps;
+        //}
     }
 }
